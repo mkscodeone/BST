ae74d721d9c7794dbb677b6f5e7fafb428824259
MKS <mks123@gmx.com>
2020-09-15 22:10:37 +0100

    added comments

--- AjaxSearch/Controllers/UserSearchController.cs
+++ AjaxSearch/Controllers/UserSearchController.cs
@@ -13,6 +13,7 @@ namespace AjaxSearch.Controllers
     {
         public List<Contact> Get(string filter)
         {
+            ///Implemented REST WebAPI
             ///For better performance limit the number of records to render in UI
             return TestData.Contacts.Where(c => c.Name.IndexOf(filter ?? string.Empty,StringComparison.CurrentCultureIgnoreCase) >= 0 ).OrderBy(nm=> nm.Name).Take(100).ToList();
         }
